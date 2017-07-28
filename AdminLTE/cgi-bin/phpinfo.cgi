21f98323d3e71948f02f8dd8a906325892643863
amosher <alexmosher03@gmail.com>
2017-07-28 19:04:06 +0200

    Initial project files

---
+++ AdminLTE/cgi-bin/phpinfo.cgi
@@ -0,0 +1,5 @@
+#!/Applications/AMPPS/php/bin/php-cgi
+<?php
+echo '<a hre="http://localhost/ampps">Back to AMPPS Home</a>';
+phpinfo();
+?>
---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+//jQuery UI sortable for the todo list
+  $(".todo-list").sortable({
+    placeholder: "sort-highlight",
+    handle: ".handle",
+    forcePlaceholderSize: true,
+    zIndex: 999999
+  });
